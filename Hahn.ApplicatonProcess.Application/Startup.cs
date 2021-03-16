c88b10d107dab3db62eb016e2c4f70f697585ceb
Mohammed Thabet <thabet084@hotmail.com>
2021-03-16 21:20:15 +0200

    commit 2

--- Hahn.ApplicatonProcess.Application/Startup.cs
+++ Hahn.ApplicatonProcess.Application/Startup.cs
@@ -29,7 +29,7 @@ using System.Threading.Tasks;
 namespace Hahn.ApplicatonProcess.Application
 {
     /// <summary>
-    /// commit 1
+    /// commit 2
     /// </summary>
     public class Startup
     {
